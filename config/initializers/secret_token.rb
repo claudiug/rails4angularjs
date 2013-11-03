6eff1cf6b71fb44fde26c29ccdc4e7a3283121a7
claudiu <claudiu.garba@gmail.com>
2013-11-03 23:29:05 +0100

    Initial commit + remove  turbolinks

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4angularjs::Application.config.secret_key_base = '45c7d54fc761828548eb6c21a1d0da8099837c9c98c3827b05a4803f49a1cb28cd21923aed3e2f91b27a8452b0de38998723fccf2a3fa02d4a66f3e4c43fb747'
